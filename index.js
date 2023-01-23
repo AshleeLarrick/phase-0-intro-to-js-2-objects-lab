e044e0593258dce8321d6008d1e44b7a5682874f
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-23 08:45:25 -0700

    Second time

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyOfEmployee = {...employee};
+    copyOfEmployee[key] = value;
+    return copyOfEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const copyOfEmployee = {...employee}
+    delete copyOfEmployee[key];
+    return copyOfEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee
+}
